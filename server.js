18a53ca77aa613c6cc811ab0d6b996f76f34db34
Blake Mason <blakelmason@gmail.com>
2019-12-17 16:45:22 -0800

    fixed music player

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var routing = require('./app/routing/htmlRoutes.js');
+var express = require('express')
+var bodyParser = require('body-parser')
+var routing = require('./app/routing/htmlRoutes.js')
 var api = require('./app/routing/apiRoutes.js')
 
-var app = express();
+var app = express()
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/app/public'))
 
-app.get('/', routing.home);
-app.get('/survey', routing.survey);
-app.get('/pokemon', api.pokemon);
+app.get('/', routing.home)
+app.get('/survey', routing.survey)
+app.get('/pokemon', api.pokemon)
 
-app.get('*', routing.home);
+app.get('*', routing.home)
 
 app.post('/survey', api.survey)
 
-
-app.listen(PORT, function () {
-  console.log('App listening on PORT ' + PORT);
-});
+app.listen(PORT, function() {
+  console.log('App listening on PORT ' + PORT)
+})
